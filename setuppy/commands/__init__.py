687330b84d08d607645910ea5e78c994ee1aa055
Matt Hoffman <mhoffman@gmail.com>
2024-10-10 01:36:02 +0100

    Add curl command.

--- setuppy/commands/__init__.py
+++ setuppy/commands/__init__.py
@@ -4,6 +4,7 @@ from typing import Type
 
 from setuppy.commands import apt
 from setuppy.commands import brew
+from setuppy.commands import curl
 from setuppy.commands import github
 from setuppy.commands import stow
 from setuppy.commands.base import BaseCommand
@@ -20,5 +21,6 @@ def register(cls: Type[BaseCommand]) -> Type[BaseCommand]:
 
 register(apt.Apt)
 register(brew.Brew)
+register(curl.Curl)
 register(github.Github)
 register(stow.Stow)
---
+++ setuppy/commands/curl.py
@@ -0,0 +1,60 @@
+"""Implementation of the curl command."""
+
+import dataclasses
+import logging
+import os
+import pathlib
+from typing import Any
+
+from setuppy.commands.base import BaseCommand
+from setuppy.commands.base import run_pipe
+from setuppy.types import SetuppyError
+
+
+@dataclasses.dataclass
+class Curl(BaseCommand):
+  """Implementation of the curl command."""
+  sources: list[str]
+  dest: str
+
+  def __call__(
+    self,
+    *,
+    facts: dict[str, Any],
+    simulate: bool,
+  ) -> bool:
+    """Run a github action."""
+    dest = pathlib.Path(self.dest.format(**facts))
+    changed = False
+
+    for s in self.sources:
+      source = s.format(**facts)
+      target = dest / os.path.basename(source)
+      suffix = "".join(target.suffixes)
+
+      if suffix == ".tar.xz":
+        taropt = "J"
+        target = target.with_suffix("").with_suffix("")
+      else:
+        raise SetuppyError('Unknown suffix "{suffix}"')
+
+      if target.exists() and target.is_dir():
+        logging.info('Target "%s" exists', target)
+        continue
+
+      changed = True
+      cmd1 = f"curl -sSL {source}"
+      cmd2 = f"tar -x{taropt}f - -C {target}"
+
+      if simulate:
+        logging.info('Skipping command "%s | %s"', cmd1, cmd2)
+        continue
+
+      target.mkdir(parents=True, exist_ok=True)
+      rc, _, _ = run_pipe(cmd1, cmd2)
+
+      if rc != 0:
+        msg = f'Error downloading target "{target}".'
+        raise SetuppyError(msg)
+
+    return changed
