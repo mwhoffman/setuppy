48dde7afa4b30b1c7088758761f9484756bb7650
Matt Hoffman <mhoffman@gmail.com>
2024-10-18 21:30:19 +0100

    Update base command docs.

--- setuppy/commands/base.py
+++ setuppy/commands/base.py
@@ -7,7 +7,12 @@ from typing import Any
 
 @dataclasses.dataclass
 class CommandResult:
-  """Return value of a command."""
+  """Return value of a command.
+
+  Properties:
+    changed: whether the command caused a change to the system.
+    facts: any updates to the system facts determined by the command.
+  """
   changed: bool
   facts: dict[str, Any] = dataclasses.field(default_factory=dict)
 
@@ -23,4 +28,15 @@ class BaseCommand(metaclass=abc.ABCMeta):
     facts: dict[str, Any],
     simulate: bool,
   ) -> CommandResult:
-    """Run the command."""
+    """Run the command.
+
+    Args:
+      facts: a dictionary containing system facts.
+      simulate: whether to simulate the command or not.
+
+    Returns:
+      A `CommandResult` whose `changed` field indicates whether the command
+      caused a change in the system. The `facts` dictionary contains updated
+      system facts which can be used by the caller to update the global
+      collection of facts.
+    """
