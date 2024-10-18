48b79853479b5f9f18d1e5cb4841068a3dd052f8
Matt Hoffman <mhoffman@gmail.com>
2024-10-18 17:41:38 +0100

    Clean up the apt test.

--- tests/test_apt.py
+++ tests/test_apt.py
@@ -8,43 +8,44 @@ from setuppy.commands.apt import Apt
 from setuppy.types import SetuppyError
 
 
-def test_command():
+@patch("setuppy.commands.apt.run_command")
+def test_command(rc):
   apt = Apt(["foo", "bar", "baz"])
 
-  # Call dpkg, but mark no changes since everything is installed. Skip the
-  # command due to the simulate flag.
-  with patch("setuppy.commands.apt.run_command") as rc:
-    rc.return_value = (0, "foo\nbar\nbaz", "")
-    rv = apt(facts={}, simulate=True)
-    dpkg = r"dpkg-query -f '${binary:Package}\n' -W"
-    assert not rv.changed
-    rc.assert_called_once_with(dpkg)
+  # Run dpkg and raise an error if that fails.
+  rc.reset_mock()
+  rc.return_value = (1, "", "")
+  with pytest.raises(SetuppyError):
+    apt(facts={}, simulate=True)
 
-  # Skip dpkg, because we've cached installed packages. Also skip running the
-  # command because of the simulate flag. Mark a change because we're missing
-  # the "baz" package.
-  with patch("setuppy.commands.apt.run_command") as rc:
-    rc.return_value = (0, "", "")
-    rv = apt(facts={"apt_packages": ["foo", "bar"]}, simulate=True)
-    dpkg = r"dpkg-query -f '${binary:Package}\n' -W"
-    assert rv.changed
-    assert not rc.called
+  # Run dpkg to find installed packages for which we'll return all of them. The
+  # command should return not changed and we should only run the dpkg command.
+  rc.reset_mock()
+  rc.return_value = (0, "foo\nbar\nbaz", "")
+  rv = apt(facts={}, simulate=False)
+  assert not rv.changed
+  rc.assert_called_once_with(r"dpkg-query -f '${binary:Package}\n' -W")
 
-  # Raise an error if the command fails.
-  with patch("setuppy.commands.apt.run_command") as rc:
-    rc.return_value = (1, "", "")
-    with pytest.raises(SetuppyError):
-      apt(facts={}, simulate=True)
+  # Skip dpkg because we have "cached" apt_packages; the baz package is missing
+  # so this results in a change, but we don't call the apt command because
+  # simulate is True.
+  rc.reset_mock()
+  rc.return_value = (0, "", "")
+  rv = apt(facts={"apt_packages": ["foo", "bar"]}, simulate=True)
+  assert rv.changed
+  assert not rc.called
 
-  # Run the command and mark a change.
-  with patch("setuppy.commands.apt.run_command") as rc:
-    rc.return_value = (0, "", "")
-    rv = apt(facts={"apt_packages": ["foo", "bar"]}, simulate=False)
-    assert rv.changed
-    rc.assert_called_once_with("apt-get -y install baz", sudo=True)
+  # Skip dpkg because we have "cached" apt_packages; try to install baz, but
+  # raise an error if the apt-get command fails.
+  rc.reset_mock()
+  rc.return_value = (1, "", "")
+  with pytest.raises(SetuppyError):
+    apt(facts={"apt_packages": ["foo", "bar"]}, simulate=False)
 
-  # Run the command and raise an error if the command fails.
-  with patch("setuppy.commands.apt.run_command") as rc:
-    rc.return_value = (1, "", "")
-    with pytest.raises(SetuppyError):
-      apt(facts={"apt_packages": ["foo", "bar"]}, simulate=False)
+  # Skip dpkg because we have "cached" apt_packages; try to install baz and make
+  # sure we properly call apt-get.
+  rc.reset_mock()
+  rc.return_value = (0, "", "")
+  rv = apt(facts={"apt_packages": ["foo", "bar"]}, simulate=False)
+  assert rv.changed
+  rc.assert_called_once_with("apt-get -y install baz", sudo=True)
