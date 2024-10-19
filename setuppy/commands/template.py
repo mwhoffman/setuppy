53d060cd1e1613e6d30d101d6834d3d94348cf73
Matt Hoffman <mhoffman@gmail.com>
2024-10-19 16:05:27 +0100

    Clean up template command and add a test.

--- setuppy/commands/template.py
+++ setuppy/commands/template.py
@@ -13,7 +13,16 @@ from setuppy.types import SetuppyError
 
 @dataclasses.dataclass
 class Template(BaseCommand):
-  """Implementation of the template command."""
+  """Recursively format and write template files from `source` to `dest`.
+
+  This command takes a `source` directory, recursively finds all files under
+  this directory, and copies them to the directory `dest`. The contents of each
+  source file is formatted using `str.format` with substitutions given by the
+  global facts dictionary.
+
+  The returned `CommandResult` will have `result.changed` set to `True` if a
+  change is made, i.e. if any file under `source` is created under the `dest`.
+  """
   source: str
   dest: str = "{home}"
 
@@ -23,27 +32,47 @@ class Template(BaseCommand):
     facts: dict[str, Any],
     simulate: bool,
   ) -> CommandResult:
-    """Run the command."""
+    """Run the template command."""
     source = pathlib.Path(self.source.format(**facts))
     dest = pathlib.Path(self.dest.format(**facts))
 
+    # Raise an error if the source exists and is not a directory.
     if not source.is_dir():
       msg = f'"{source.absolute()}" does not exist or is not a directory.'
       raise SetuppyError(msg)
 
+    # We haven't made any changes yet.
     changed = False
+
+    # NOTE: In order to support py3.11 we can't use source.walk() which was only
+    # introduced in py3.12.
+
+    # Walk the path.
     for path, _, files in os.walk(source):
       for f in files:
         file = pathlib.Path(path) / f
         target = dest / file.relative_to(source)
 
         if target.exists():
+          # Raise an error if the target exists and is not a file.
+          if target.is_dir():
+            msg = f'"{target.absolute()}" exists and is not a file.'
+            raise SetuppyError(msg)
+
+          # Otherwise we'll just log and skip the target.
           logging.info('Target "%s" exists', target)
 
+          # TODO: Should we try and evaluate whether the file's contents would
+          # be changed and only skip if they WOULDN'T be changed.
+
         else:
+          # Target doesn't exist so we'll create it.
           changed = True
           logging.info('Creating "%s"', target)
+
+          # ... unless we're simulating. So don't actually make the change.
           if not simulate:
+            target.parent.mkdir(parents=True)
             target.write_text(file.read_text().format(**facts))
 
     return CommandResult(changed=changed)
---
+++ tests/test_template.py
@@ -0,0 +1,61 @@
+"""Test for the template command."""
+
+import pathlib
+
+import pytest
+from pyfakefs.fake_filesystem import FakeFilesystem
+
+from setuppy.commands.template import Template
+from setuppy.types import SetuppyError
+
+
+def test_curl(fs: FakeFilesystem):
+  source = "/source"
+  dest = "/dest"
+  template = Template(source, dest)
+
+  # Raise an error if source exists, but is a file.
+  fs.reset()
+  fs.create_file("/source")
+  with pytest.raises(SetuppyError):
+    template(facts={}, simulate=False)
+
+  # No changes should occur if the source directory is empty.
+  fs.reset()
+  fs.create_dir(source)
+  rv = template(facts={}, simulate=False)
+  assert not rv.changed
+  # We didn't create the destination directory so nothing should exist. But
+  # we'll check both just to be pedantic.
+  assert not pathlib.Path(dest).exists()
+  assert list(pathlib.Path(dest).glob("*")) == []
+
+  # Raise an error if an existing target is a directory not a file.
+  fs.reset()
+  fs.create_file(source + "/foo", contents="foo")
+  fs.create_dir(dest + "/foo")
+  with pytest.raises(SetuppyError):
+    template(facts={}, simulate=False)
+
+  # No changes should occur if the target already exists.
+  fs.reset()
+  fs.create_file(source + "/foo", contents="foo")
+  fs.create_file(dest + "/foo", contents="bar")
+  rv = template(facts={}, simulate=False)
+  assert not rv.changed
+  assert pathlib.Path(dest + "/foo").read_text() == "bar"
+
+  # Make sure we create the file, do the templating using format, and that we
+  # can create intermediate directories.
+  fs.reset()
+  fs.create_file(source + "/foo/bar", contents="{foo}")
+  rv = template(facts={"foo": "bar"}, simulate=False)
+  assert rv.changed
+  assert pathlib.Path(dest + "/foo/bar").read_text() == "bar"
+
+  # Same as above, but simulate so the file shouldn't exist.
+  fs.reset()
+  fs.create_file(source + "/foo/bar", contents="{foo}")
+  rv = template(facts={"foo": "bar"}, simulate=True)
+  assert rv.changed
+  assert not pathlib.Path(dest + "/foo/bar").exists()
