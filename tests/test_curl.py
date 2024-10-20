49d82cf96a6f8a6cf73de78db76273151a824269
Matt Hoffman <mhoffman@gmail.com>
2024-10-20 13:40:37 +0100

    Split curl test.

--- tests/test_curl.py
+++ tests/test_curl.py
@@ -1,69 +1,91 @@
 """Test for the curl command."""
 
-from unittest.mock import patch
+from collections.abc import Iterable
+from unittest import mock
 
 import pytest
+from pyfakefs.fake_filesystem import FakeFilesystem
 
 from setuppy.commands.curl import Curl
 from setuppy.types import SetuppyError
 
 
-@patch("setuppy.commands.curl.run_pipe")
-def test_curl(rp, fs):
-  # Create a command with an invalid suffix.
-  url = "http://foo.com/bar.tar.gz"
-  dest = "/"
-  curl = Curl([url], dest)
+URL = "http://foo.com/bar.tar.xz"
+DEST = "/"
+TARGET = "/bar"
 
-  # Make the run_pipe command return success so that if we do call it we can
-  # raise an assertion error rather than having it fail within the command call.
-  rp.return_value = (0, "", "")
 
-  # Running the invalid command should raise an error.
-  rp.reset_mock()
-  fs.reset()
+@pytest.fixture
+def rp() -> Iterable[mock.MagicMock]:
+  patcher = mock.patch("setuppy.commands.curl.run_pipe")
+  run_command = patcher.start()
+  run_command.return_value = (0, "", "")
+  yield run_command
+  patcher.stop()
+
+
+def test_invalid_suffix(
+  rp: mock.MagicMock,
+):
+  # A command with an invalid suffix should raise an exception.
+  curl = Curl(sources=["http://foo.com/bar.tar.gz"], dest="/")
   with pytest.raises(SetuppyError):
     curl(facts={}, simulate=False)
   assert not rp.called
 
-  # Create a valid command.
-  url = "http://foo.com/bar.tar.xz"
-  dest = "/"
-  curl = Curl([url], dest)
 
+def test_exists(
+  rp: mock.MagicMock,
+  fs: FakeFilesystem,
+):
   # Do nothing if the target directory exists.
-  rp.reset_mock()
-  fs.reset()
-  fs.create_dir("/bar")
+  fs.create_dir(TARGET)
+  curl = Curl([URL], dest=DEST)
   rv = curl(facts={}, simulate=False)
   assert not rp.called
   assert not rv.changed
 
+
+def test_exists_is_file(
+  rp: mock.MagicMock,
+  fs: FakeFilesystem,
+):
   # Raise an error if the target exists but is a file.
-  rp.reset_mock()
-  fs.reset()
   fs.create_file("/bar")
+  curl = Curl([URL], dest=DEST)
   with pytest.raises(SetuppyError):
     curl(facts={}, simulate=False)
   assert not rp.called
 
+
+def test_command(
+  rp: mock.MagicMock,
+  fs: FakeFilesystem,  # noqa: ARG001
+):
   # Make sure we run the command.
-  rp.reset_mock()
-  fs.reset()
+  curl = Curl([URL], dest=DEST)
   rv = curl(facts={}, simulate=False)
   assert rv.changed
-  rp.assert_called_once_with(f"curl -sSL {url}", "tar -xJf - -C /bar")
+  rp.assert_called_once_with(f"curl -sSL {URL}", f"tar -xJf - -C {TARGET}")
 
+
+def test_simulate(
+  rp: mock.MagicMock,
+  fs: FakeFilesystem,  # noqa: ARG001
+):
   # Same as above but we'll simulate so the command shouldn't be called.
-  rp.reset_mock()
-  fs.reset()
+  curl = Curl([URL], dest=DEST)
   rv = curl(facts={}, simulate=True)
   assert rv.changed
   assert not rp.called
 
+
+def test_command_fails(
+  rp: mock.MagicMock,
+  fs: FakeFilesystem,  # noqa: ARG001
+):
   # Same as above but raise an exception if the command fails.
-  rp.reset_mock()
   rp.return_value = (1, "", "")
-  fs.reset()
+  curl = Curl([URL], dest=DEST)
   with pytest.raises(SetuppyError):
     curl(facts={}, simulate=False)
