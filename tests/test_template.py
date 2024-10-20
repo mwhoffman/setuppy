2c81ebd8073a801fc479473f217f9603102c1886
Matt Hoffman <mhoffman@gmail.com>
2024-10-20 13:49:20 +0100

    Split template test.

--- tests/test_template.py
+++ tests/test_template.py
@@ -9,53 +9,64 @@ from setuppy.commands.template import Template
 from setuppy.types import SetuppyError
 
 
-def test_template(fs: FakeFilesystem):
-  source = "/source"
-  dest = "/dest"
-  template = Template(source, dest)
+SOURCE = "/source"
+DEST = "/dest"
 
-  # Raise an error if source exists, but is a file.
-  fs.reset()
-  fs.create_file("/source")
+
+def test_exists_is_file(fs: FakeFilesystem):
+  # Raise an error if SOURCE exists, but is a file.
+  fs.create_file(SOURCE)
+  template = Template(SOURCE, DEST)
   with pytest.raises(SetuppyError):
     template(facts={}, simulate=False)
 
-  # No changes should occur if the source directory is empty.
-  fs.reset()
-  fs.create_dir(source)
+
+def test_source_empty(fs: FakeFilesystem):
+  # No changes should occur if the SOURCE directory is empty.
+  fs.create_dir(SOURCE)
+  template = Template(SOURCE, DEST)
   rv = template(facts={}, simulate=False)
   assert not rv.changed
   # We didn't create the destination directory so nothing should exist. But
   # we'll check both just to be pedantic.
-  assert not pathlib.Path(dest).exists()
-  assert list(pathlib.Path(dest).glob("*")) == []
+  assert not pathlib.Path(DEST).exists()
+  assert list(pathlib.Path(DEST).glob("*")) == []
 
+
+def test_target_exists_is_file(fs: FakeFilesystem):
   # Raise an error if an existing target is a directory not a file.
-  fs.reset()
-  fs.create_file(source + "/foo", contents="foo")
-  fs.create_dir(dest + "/foo")
+  fs.create_file(SOURCE + "/foo", contents="foo")
+  fs.create_dir(DEST + "/foo")
+  template = Template(SOURCE, DEST)
   with pytest.raises(SetuppyError):
     template(facts={}, simulate=False)
 
+
+def test_target_exists(fs: FakeFilesystem):
   # No changes should occur if the target already exists.
-  fs.reset()
-  fs.create_file(source + "/foo", contents="foo")
-  fs.create_file(dest + "/foo", contents="bar")
+  fs.create_file(SOURCE + "/foo", contents="foo")
+  fs.create_file(DEST + "/foo", contents="bar")
+  template = Template(SOURCE, DEST)
   rv = template(facts={}, simulate=False)
   assert not rv.changed
-  assert pathlib.Path(dest + "/foo").read_text() == "bar"
+  assert pathlib.Path(DEST + "/foo").read_text() == "bar"
 
+
+def test_template(fs: FakeFilesystem):
   # Make sure we create the file, do the templating using format, and that we
   # can create intermediate directories.
-  fs.reset()
-  fs.create_file(source + "/foo/bar", contents="{foo}")
+  fs.create_file(SOURCE + "/foo/bar", contents="{foo}")
+  template = Template(SOURCE, DEST)
   rv = template(facts={"foo": "bar"}, simulate=False)
   assert rv.changed
-  assert pathlib.Path(dest + "/foo/bar").read_text() == "bar"
+  assert pathlib.Path(DEST + "/foo/bar").read_text() == "bar"
 
+
+def test_simulate(fs: FakeFilesystem):
   # Same as above, but simulate so the file shouldn't exist.
   fs.reset()
-  fs.create_file(source + "/foo/bar", contents="{foo}")
+  fs.create_file(SOURCE + "/foo/bar", contents="{foo}")
+  template = Template(SOURCE, DEST)
   rv = template(facts={"foo": "bar"}, simulate=True)
   assert rv.changed
-  assert not pathlib.Path(dest + "/foo/bar").exists()
+  assert not pathlib.Path(DEST + "/foo/bar").exists()
