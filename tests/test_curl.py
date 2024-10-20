0e6d9b01ed2955940ed9d916ec54ae6a08002a37
Matt Hoffman <mhoffman@gmail.com>
2024-10-20 13:51:05 +0100

    Use better name for run_pipe fixture in curl test.

--- tests/test_curl.py
+++ tests/test_curl.py
@@ -16,7 +16,7 @@ TARGET = "/bar"
 
 
 @pytest.fixture
-def rp() -> Iterable[mock.MagicMock]:
+def run_pipe() -> Iterable[mock.MagicMock]:
   patcher = mock.patch("setuppy.commands.curl.run_pipe")
   run_command = patcher.start()
   run_command.return_value = (0, "", "")
@@ -25,29 +25,29 @@ def rp() -> Iterable[mock.MagicMock]:
 
 
 def test_invalid_suffix(
-  rp: mock.MagicMock,
+  run_pipe: mock.MagicMock,
 ):
   # A command with an invalid suffix should raise an exception.
   curl = Curl(sources=["http://foo.com/bar.tar.gz"], dest="/")
   with pytest.raises(SetuppyError):
     curl(facts={}, simulate=False)
-  assert not rp.called
+  assert not run_pipe.called
 
 
 def test_exists(
-  rp: mock.MagicMock,
+  run_pipe: mock.MagicMock,
   fs: FakeFilesystem,
 ):
   # Do nothing if the target directory exists.
   fs.create_dir(TARGET)
   curl = Curl([URL], dest=DEST)
   rv = curl(facts={}, simulate=False)
-  assert not rp.called
+  assert not run_pipe.called
   assert not rv.changed
 
 
 def test_exists_is_file(
-  rp: mock.MagicMock,
+  run_pipe: mock.MagicMock,
   fs: FakeFilesystem,
 ):
   # Raise an error if the target exists but is a file.
@@ -55,37 +55,39 @@ def test_exists_is_file(
   curl = Curl([URL], dest=DEST)
   with pytest.raises(SetuppyError):
     curl(facts={}, simulate=False)
-  assert not rp.called
+  assert not run_pipe.called
 
 
 def test_command(
-  rp: mock.MagicMock,
+  run_pipe: mock.MagicMock,
   fs: FakeFilesystem,  # noqa: ARG001
 ):
   # Make sure we run the command.
   curl = Curl([URL], dest=DEST)
   rv = curl(facts={}, simulate=False)
   assert rv.changed
-  rp.assert_called_once_with(f"curl -sSL {URL}", f"tar -xJf - -C {TARGET}")
+  cmd1 = f"curl -sSL {URL}"
+  cmd2 = f"tar -xJf - -C {TARGET}"
+  run_pipe.assert_called_once_with(cmd1, cmd2)
 
 
 def test_simulate(
-  rp: mock.MagicMock,
+  run_pipe: mock.MagicMock,
   fs: FakeFilesystem,  # noqa: ARG001
 ):
   # Same as above but we'll simulate so the command shouldn't be called.
   curl = Curl([URL], dest=DEST)
   rv = curl(facts={}, simulate=True)
   assert rv.changed
-  assert not rp.called
+  assert not run_pipe.called
 
 
 def test_command_fails(
-  rp: mock.MagicMock,
+  run_pipe: mock.MagicMock,
   fs: FakeFilesystem,  # noqa: ARG001
 ):
   # Same as above but raise an exception if the command fails.
-  rp.return_value = (1, "", "")
+  run_pipe.return_value = (1, "", "")
   curl = Curl([URL], dest=DEST)
   with pytest.raises(SetuppyError):
     curl(facts={}, simulate=False)
